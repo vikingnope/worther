069447e46685b4fd0d7f684cf5a275d39a275d18
vikingnope <aidsch0605@outlook.com>
2022-05-21 10:19:26 +0200

    Added tailwind CSS to project but not functional

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import About from './About.js';
 import Contact from './Contact.js';
 
+
 function App() {
   return (
     <Router>
