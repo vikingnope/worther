8dd04402d06c9c020187beb16673fd1c50da1272
Aiden Schembri <aidsch0605@outlook.com>
2022-06-01 09:34:53 +0200

    Removed all code from contact.css to contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,14 +1,13 @@
-import '../styles/Contact.css';
 import React from 'react';
 
 const Contact = () => {
   return (
-    <div className="App">
-        <header className="App-header">
-            <p className="App-text">
+    <div className="text-center">
+        <header className="bg-black min-h-screen flex flex-col items-center justify-center text-white">
+            <p className="-mt-96 text-6xl">
                 Contact Us
             </p>
-            <input className="App-input-name" placeholder="Name"></input>
+            <input className="bg-white border-solid rounded-md text-2xl outline-none border-blue-700 text-black" placeholder="Name"></input>
         </header>
     </div>
   );
