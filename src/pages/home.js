b0872de066f108c39b4aaacc4f81ee9d4aa7018a
vikingnope <aidsch0605@outlook.com>
2022-06-05 12:46:55 +0200

    Rounded logo, changed some margins

--- src/pages/home.js
+++ src/pages/home.js
@@ -7,14 +7,14 @@ const Home = () => {
   return (
     <div className="text-center select-none">
         <header className="bg-black min-h-screen flex flex-col items-center justify-center text-white">
-            <img src={logo} className="m-20 App-logo" alt="logo" />
+            <img src={logo} className="m-5 App-logo rounded-full" alt="logo" />
             <p className="uppercase font-bold text-5xl">
                 Worther, coming soon!
             </p>
             {/*<a href="/map" className="uppercase font-bold text-4xl underline m-3 text-blue-400">
                 Map
              </a>*/}
-            <a href="/about" className="uppercase font-bold text-4xl underline">
+            <a href="/about" className="uppercase font-bold text-4xl underline mt-5">
                 About
             </a>
         </header>
