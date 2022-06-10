ac059cf2a4cdb57cbc407c3fea3972e6f0a76622
vikingnope <aidsch0605@outlook.com>
2022-06-11 00:10:16 +0200

    Started implementation of rainviewer API

--- src/components/rainViewerData.js
+++ src/components/rainViewerData.js
@@ -1,12 +1,25 @@
-import axios from 'axios';
-import { useEffect } from 'react';
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-const baseURL = 'https://api.rainviewer.com/public/weather-maps.json';
+const baseURL = "https://api.rainviewer.com/public/weather-maps.json";
+
+let url = '';
+let path = '';
 
 export const GetRainViewerData = () => {
-    useEffect(() => {
-        axios.get(baseURL).then(response => {
-            console.log(response.data)
-        })
-    })
-}
+
+  axios.get(baseURL)
+  .then(function (response) {
+    //console.log(response);
+    url = response.data.host;
+    path = response.data.radar.past;
+    console.log(url);
+    console.log(path);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });  
+};
