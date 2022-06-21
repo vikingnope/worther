7de6d0eabae397a08454416f5549eede32fe41f3
vikingnope <aidsch0605@outlook.com>
2022-06-21 13:14:20 +0200

    Changed text sizes in home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -8,13 +8,13 @@ export default function Home() {
         <div className="text-center select-none">
             <header className="bg-black min-h-screen flex flex-col items-center justify-center text-white">
                 <img src={logo} className="m-5 App-logo rounded-full" alt="logo" />
-                <p className="uppercase font-bold text-5xl">
-                    Worther, coming soon!
+                <p className="uppercase font-bold text-7xl">
+                    Worther
                 </p>
-                <a href="/map" className="uppercase font-bold text-4xl underline m-3 text-blue-400">
+                <a href="/map" className="uppercase font-bold text-3xl underline m-3 text-blue-400">
                     Map
                 </a>
-                <a href="/about" className="uppercase font-bold text-4xl underline">
+                <a href="/about" className="uppercase font-bold text-3xl underline">
                     About
                 </a>
             </header>
