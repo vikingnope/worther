8dc876f184342205b2506bf348730f4b95e04447
vikingnope <aidsch0605@outlook.com>
2022-06-21 21:34:25 +0200

    Added more features to header

--- src/components/utils/header.jsx
+++ src/components/utils/header.jsx
@@ -1,15 +1,19 @@
-
+import logo from '../../resources/logoSmall.png'
 
 export const Header = () => {
     return (
-        <div className="absolute top-2.5 right-0">
-            <a href="/map" className="relative uppercase text-2xl mr-10 hover:text-green-300">
-                Map
-            </a>
-            <a href="/about" className="relative uppercase text-2xl mr-40 hover:text-green-300">
-                About
-            </a>
+        <div className="absolute inset-x-0 top-0 bg-neutral-800 h-14 w-full border-y border-zinc-600">
+            <div className="fixed top-2 left-0">
+                <img src={logo} className="-mt-2.5 ml-1.5 scale-75 rounded-full" alt="logo" />
+            </div>
+            <div className="fixed top-3 right-0">
+                <a href="/map" className="relative uppercase text-2xl mr-10 hover:text-green-300">
+                    Map
+                </a>
+                <a href="/about" className="relative uppercase text-2xl mr-40 hover:text-green-300">
+                    About
+                </a>
+            </div>
         </div>
-       
     )
 };
