5024047e2cc2442fcebcd631f928469e0e495d3f
vikingnope <aidsch0605@outlook.com>
2022-06-21 20:37:26 +0200

    Updated bbox

--- src/components/satelliteData.jsx
+++ src/components/satelliteData.jsx
@@ -6,8 +6,8 @@ export const SatelliteData = () => {
         <TileLayer 
             zIndex={2} 
             opacity={0.1} 
-            tileSize={1905}
-            url={"https://view.eumetsat.int/geoserver/ows?service=WMS&request=GetMap&version=1.3.0&layers=mumi:wideareacoverage_rgb_natural&height=1920&width=1920&crs=EPSG:4326&format=image/png&access_token=96441f96-86c0-3285-a906-d39abb322f20&bbox=-77,-77,77,77"}
+            tileSize={1920}
+            url={"https://view.eumetsat.int/geoserver/ows?service=WMS&request=GetMap&version=1.3.0&layers=mumi:wideareacoverage_rgb_natural&height=1920&width=1920&crs=EPSG:4326&format=image/jpeg&access_token=96441f96-86c0-3285-a906-d39abb322f20&bbox=-180,-180,180,180"}
         />
     )
 }
