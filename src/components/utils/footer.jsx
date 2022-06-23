b13949ad7cb7d9442520c319a9417a710db259ff
vikingnope <aidsch0605@outlook.com>
2022-06-23 22:06:05 +0200

    Made footer icons smaller

--- src/components/utils/footer.jsx
+++ src/components/utils/footer.jsx
@@ -4,12 +4,12 @@ import { BsGithub } from 'react-icons/bs';
 export const Footer = () => {
     return(
         <div className="absolute inset-x-0 bottom-0 bg-zinc-800 h-10 w-full border-y border-zinc-600">
-            <div className="relative -bottom-1 flex gap-x-4 justify-center">
+            <div className="relative -bottom-1.5 flex gap-x-4 justify-center">
                 <a className="hover:text-green-300" href="https://discord.gg/n6xr3ZWM8J" target="_blank" rel="noreferrer">
-                    <FaDiscord size="30"/>
+                    <FaDiscord size="26"/>
                 </a>
                 <a className="hover:text-green-300" href="https://github.com/vikingnope/worther" target="_blank" rel="noreferrer">
-                    <BsGithub size="30"/>
+                    <BsGithub size="26"/>
                 </a>
             </div>
         </div> 
