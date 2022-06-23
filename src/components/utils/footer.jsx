acbc927097a686ede6c0dce6c861fb0e4008699d
vikingnope <aidsch0605@outlook.com>
2022-06-23 22:05:11 +0200

    Added border to footer

--- src/components/utils/footer.jsx
+++ src/components/utils/footer.jsx
@@ -3,7 +3,7 @@ import { BsGithub } from 'react-icons/bs';
 
 export const Footer = () => {
     return(
-        <div className="absolute inset-x-0 bottom-0 bg-zinc-800 h-10 w-full">
+        <div className="absolute inset-x-0 bottom-0 bg-zinc-800 h-10 w-full border-y border-zinc-600">
             <div className="relative -bottom-1 flex gap-x-4 justify-center">
                 <a className="hover:text-green-300" href="https://discord.gg/n6xr3ZWM8J" target="_blank" rel="noreferrer">
                     <FaDiscord size="30"/>
