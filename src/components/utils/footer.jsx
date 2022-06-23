de4fd35cbbc657079f2d3b4c1d925e38ea81354a
vikingnope <aidsch0605@outlook.com>
2022-06-23 22:03:48 +0200

    Added rel="noreferrer"

--- src/components/utils/footer.jsx
+++ src/components/utils/footer.jsx
@@ -5,10 +5,10 @@ export const Footer = () => {
     return(
         <div className="absolute inset-x-0 bottom-0 bg-zinc-800 h-10 w-full">
             <div className="relative -bottom-1 flex gap-x-4 justify-center">
-                <a className="hover:text-green-300" href="https://discord.gg/n6xr3ZWM8J" target="_blank">
+                <a className="hover:text-green-300" href="https://discord.gg/n6xr3ZWM8J" target="_blank" rel="noreferrer">
                     <FaDiscord size="30"/>
                 </a>
-                <a className="hover:text-green-300" href="https://github.com/vikingnope/worther" target="_blank">
+                <a className="hover:text-green-300" href="https://github.com/vikingnope/worther" target="_blank" rel="noreferrer">
                     <BsGithub size="30"/>
                 </a>
             </div>
