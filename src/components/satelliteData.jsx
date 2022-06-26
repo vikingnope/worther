039987e234731aa81e7a900e9a4cbea91d86855d
Aiden Schembri <aidsch0605@outlook.com>
2022-06-26 14:12:42 +0200

    Started to fix issue of Bbox and tilesize

--- src/components/satelliteData.jsx
+++ src/components/satelliteData.jsx
@@ -1,13 +1,28 @@
-import { TileLayer } from 'react-leaflet';
+import { TileLayer, useMap } from 'react-leaflet';
+import { useEffect, useState } from 'react';
 
 export const SatelliteData = () => {
-    
+    const [Bbox, setBbox] = useState();
+    const [size, setSize] = useState();
+
+    const map = useMap()
+
+    useEffect(() => {
+        setBbox(map.getBounds().toBBoxString());
+        setSize(map.getSize());
+    }, [map]);
+
     return (
-        <TileLayer 
-            zIndex={2} 
-            opacity={0.1} 
-            tileSize={1920}
-            url={"https://view.eumetsat.int/geoserver/ows?service=WMS&request=GetMap&version=1.3.0&layers=mumi:wideareacoverage_rgb_natural&height=1920&width=1920&crs=EPSG:4326&format=image/jpeg&access_token=96441f96-86c0-3285-a906-d39abb322f20&bbox=-180,-180,180,180"}
-        />
+        <>
+            {
+                (Bbox !== undefined && size !== undefined) ?
+                    <TileLayer 
+                    zIndex={2} 
+                    opacity={0.1}
+                    tileSize={size}
+                    url={`https://view.eumetsat.int/geoserver/ows?service=WMS&request=GetMap&version=1.3.0&layers=mumi:wideareacoverage_rgb_natural&height=${size.y}&width=${size.x}&crs=EPSG:4326&format=image/jpeg&access_token=96441f96-86c0-3285-a906-d39abb322f20&bbox=${Bbox}`}
+                    /> : <> </>
+            }   
+        </>
     )
-}
+};
