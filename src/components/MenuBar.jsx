4742907368d9395ef1a51faccca901cd9d87ed50
Aiden Schembri <aidsch0605@outlook.com>
2022-06-26 15:45:07 +0200

    More fixes to opacity bar

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -2,8 +2,8 @@
 
 export const MenuBar = (props) => {
     return(
-        <div className="relative ml-3 mt-24 cursor-default" id="opacityBar">
-            <div className="absolute w-30 h-15 zIndex z-50 bg-white border-2 border-neutral-800 rounded-lg">
+        <div className="relative leaflet-top leaflet-left">
+            <div className="absolute ml-3 mt-24 w-30 h-15 z-50 bg-white border-2 border-neutral-800 rounded-lg cursor-default" id="opacityBar">
                 <p className="font-bold text-sm ml-0.5">
                     Weather opacity (only click):
                 </p>
