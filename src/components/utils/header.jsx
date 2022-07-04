19ceee5d0b4ae62479c4f230e88ce482f5ecaffd
vikingnope <aidsch0605@outlook.com>
2022-07-04 14:10:52 +0200

    Added z index

--- src/components/utils/header.jsx
+++ src/components/utils/header.jsx
@@ -2,7 +2,7 @@ import logo from '../../resources/logoSmall.png'
 
 export const Header = () => {
     return (
-        <div className="absolute inset-x-0 top-0 bg-neutral-800 h-14 w-full border-y border-zinc-600">
+        <div className="absolute inset-x-0 top-0 bg-neutral-800 h-14 w-full border-y border-zinc-600 z-50">
             <div className="fixed top-2 left-0">
                 <img src={logo} className="-mt-2.5 ml-2.5 scale-75 rounded-full" alt="logo" />
             </div>
