f7a3b9eb2e6657d7616744bc939ce9cd26a33e25
vikingnope <aidsch0605@outlook.com>
2022-07-22 17:01:40 +0200

    Changed structure of code in contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 
 export default function Contact() {
   return (
-    <main className="text-center">
-        <section className="bg-black min-h-screen flex flex-col items-center justify-center text-white">
-            <p className="-mt-96 text-6xl">
-                Contact Us
-            </p>
-            <input className="bg-white border-solid rounded-md text-2xl outline-none border-blue-700 text-black mt-5" placeholder="Name"></input>
-        </section>
+    <main className="text-center bg-black min-h-screen flex flex-col items-center justify-center text-white">
+      <p className="-mt-96 text-6xl">
+          Contact Us
+      </p>
+      <input 
+        className="bg-white border-solid rounded-md text-2xl outline-none border-blue-700 text-black mt-5" 
+        placeholder="Name">
+      </input>
     </main>
   );
 }
