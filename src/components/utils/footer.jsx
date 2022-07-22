7761b70528d4deffc66c312d44c0eb487576bf92
vikingnope <aidsch0605@outlook.com>
2022-07-22 16:48:34 +0200

    Made html header and footer better

--- src/components/utils/footer.jsx
+++ src/components/utils/footer.jsx
@@ -3,16 +3,16 @@ import { BsGithub } from 'react-icons/bs';
 
 export const Footer = () => {
     return(
-        <div className="absolute inset-x-0 bottom-0 bg-zinc-800 h-10 w-full border-y border-zinc-600">
-            <span className="relative -bottom-1.5 flex gap-x-4 justify-center">
+        <footer className="absolute inset-x-0 bottom-0 bg-zinc-800 h-10 w-full border-y border-zinc-600">
+            <section className="relative -bottom-1.5 flex gap-x-4 justify-center">
                 <a className="hover:text-green-300" href="https://discord.gg/n6xr3ZWM8J" target="_blank" rel="noreferrer">
                     <FaDiscord size="26"/>
                 </a>
                 <a className="hover:text-green-300" href="https://github.com/vikingnope/worther" target="_blank" rel="noreferrer">
                     <BsGithub size="26"/>
                 </a>
-            </span>
-        </div> 
+            </section>
+        </footer> 
     )
 };
     
--- src/components/utils/header.jsx
+++ src/components/utils/header.jsx
@@ -2,18 +2,18 @@ import logo from '../../resources/logoSmall.png'
 
 export const Header = () => {
     return (
-        <div className="absolute inset-x-0 top-0 bg-neutral-800 h-14 w-full border-y border-zinc-600 z-50">
-            <span className="fixed top-2 left-0">
+        <header className="absolute inset-x-0 top-0 bg-neutral-800 h-14 w-full border-y border-zinc-600 z-50">
+            <section className="fixed top-2 left-0">
                 <img src={logo} className="-mt-2.5 ml-2.5 scale-75 rounded-full" alt="logo" />
-            </span>
-            <span className="fixed top-3 right-0">
+            </section>
+            <section className="fixed top-3 right-0">
                 <a href="/map" className="relative uppercase text-2xl mr-7 hover:text-green-300">
                     Map
                 </a>
                 <a href="/about" className="relative uppercase text-2xl mr-12 hover:text-green-300">
                     About
                 </a>
-            </span>
-        </div>
+            </section>
+        </header>
     )
 };
