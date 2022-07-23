986c6f700db464a05e3684533b926198a569db6f
Aiden Schembri <aidsch0605@outlook.com>
2022-07-23 07:14:54 +0200

    Arranged form syntax

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -3,17 +3,19 @@ import {AiOutlineClose} from 'react-icons/ai';
 
 export const MenuBar = (props) => {
     return(
-        <div className="relative leaflet-top leaflet-left">
-            <form className="absolute ml-3 mt-24 w-52 h-15 z-50 bg-white border-2 border-neutral-800 rounded-lg cursor-default" id="opacityBar">
+        <section className="relative leaflet-top leaflet-left">
+            <div className="absolute ml-3 mt-24 w-52 h-15 z-50 bg-white border-2 border-neutral-800 rounded-lg cursor-default" id="opacityBar">
                 <div className="absolute ml-44 mt-px">
                     <AiOutlineSearch size="23" />
                     {/* {search.length === 0 ? <AiOutlineSearch size="23"/> : <AiOutlineClose size="23"/>} */}
                 </div>
-                <input
-                    className="ml-0.5 border-b-2 border-sky-900 w-44 h-7 text-base font-bold indent-0.5 border-dashed outline-none"
-                    type="text"
-                    placeholder="Location"
-                />
+                <form>
+                    <input
+                        className="ml-0.5 border-b-2 border-sky-900 w-44 h-7 text-base font-bold indent-0.5 border-dashed outline-none"
+                        type="text"
+                        placeholder="Location"
+                    />
+                </form>
                 <p className="font-bold text-sm ml-0.5">
                     Weather opacity (only click):
                 </p>
@@ -25,7 +27,7 @@ export const MenuBar = (props) => {
                     max={100}
                     onChange={event => props.onWeatherOpacityChange && props.onWeatherOpacityChange(Number(event.target.value) / 100)}
                 />
-            </form>
-        </div>
+            </div>
+        </section>
     )
 };
