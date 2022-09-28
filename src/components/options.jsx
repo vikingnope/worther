8328be649943c4f1e09147520c484936e0287cf8
Aiden Schembri <aidsch0605@outlook.com>
2022-09-28 09:05:38 +0200

    Centred icon in button

--- src/components/options.jsx
+++ src/components/options.jsx
@@ -4,8 +4,8 @@ import {BsFillMoonFill} from 'react-icons/bs';
 export const Options = () => {
   return (
     <div className="leaflet-bottom leaflet-right w-16 h-96">
-        <button type="button" className="cursor-pointer absolute w-7 h-7 bottom-12 rounded-md border-black border-solid border-2 left-6">
-            <BsFillMoonFill size="13"/>
+        <button onclick="" type="button" className="cursor-pointer absolute w-8 h-8 bottom-12 rounded-md bg-white border-black border-solid border-2 left-6">
+            <BsFillMoonFill className="block m-auto" size="15"/>
         </button>
     </div>
   )
