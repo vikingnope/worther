7d18fe21ac2b4050df14b322c2e28ac0fcd8318b
vikingnope <aidsch0605@outlook.com>
2022-10-11 23:41:02 +0200

    Added gap

--- src/components/utils/footer.jsx
+++ src/components/utils/footer.jsx
@@ -4,7 +4,7 @@ import { BsGithub } from 'react-icons/bs';
 export const Footer = () => {
     return(
         <footer className="absolute inset-x-0 bottom-0 bg-zinc-800 h-10 w-full border-y border-zinc-600">
-            <nav className="relative -bottom-1.5 flex gap-x-4 justify-center">
+            <nav className="relative -bottom-1.5 flex gap-x-5 justify-center">
                 <a className="hover:text-green-300" href="https://discord.gg/n6xr3ZWM8J" target="_blank" rel="noreferrer">
                     <FaDiscord size="26"/>
                 </a>
