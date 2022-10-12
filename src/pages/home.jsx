d7df4bfb9ec8068c3b896d8c0d51947dad02c7a8
vikingnope <aidsch0605@outlook.com>
2022-10-12 17:55:45 +0200

    Fixed image not being centered

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -10,7 +10,7 @@ export default function Home() {
         <div className="text-center select-none bg-black min-h-screen flex flex-col items-center justify-center text-white">
             <Header choice={'home'}/>
             <main>
-                <img src={logo} className="m-7 ml-24 App-logo rounded-full" alt="logo" />
+                <img src={logo} className="mx-auto mb-7 App-logo rounded-full" alt="logo" />
                 <p className="uppercase font-bold text-7xl">
                     Worther
                 </p>
