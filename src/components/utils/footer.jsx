f7e1c74b11a4a5b3da496c7d9c48d58601a414d3
vikingnope <aidsch0605@outlook.com>
2022-10-12 20:28:26 +0200

    try fix for footer

--- src/components/utils/footer.jsx
+++ src/components/utils/footer.jsx
@@ -3,7 +3,7 @@ import { BsGithub } from 'react-icons/bs';
 
 export const Footer = () => {
     return(
-        <footer className="absolute inset-x-0 bottom-0 bg-zinc-800 h-10 w-full border-y border-zinc-600">
+        <footer className="inset-x-0 bottom-0 bg-zinc-800 h-10 w-full border-y border-zinc-600">
             <nav className="relative -bottom-1.5 flex gap-x-5 justify-center">
                 <a className="hover:text-green-300" href="https://discord.gg/n6xr3ZWM8J" target="_blank" rel="noreferrer">
                     <FaDiscord size="26"/>
