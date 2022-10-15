ddbe82318815ea88b2b7ba847c11ea83714206fd
vikingnope <aidsch0605@outlook.com>
2022-10-15 15:42:46 +0200

    Added icons in mobile bar

--- src/components/utils/mobileDropdown.jsx
+++ src/components/utils/mobileDropdown.jsx
@@ -1,14 +1,39 @@
 import { MdOutlineSegment } from 'react-icons/md';
 import { useState } from 'react';
 import { MdArrowLeft, MdOutlineArrowDropDown } from 'react-icons/md';
+import { AiFillHome } from 'react-icons/ai';
+import { BsFillMapFill, BsCloudSunFill } from 'react-icons/bs';
+import { HiInformationCircle } from 'react-icons/hi';
 
 export const Dropdown = (props) => {
     const [toggle, setToggle ] = useState(false);
 
     const Navigations = (text, path) => {
         return(
-            <a href={path} className="w-44 h-14 flex justify-center uppercase text-2xl border-y-2 border-x-2 border-zinc-600 mr-1 bg-stone-900 indent-2 ">
-                {text}
+            <a href={path} className="w-44 h-14 flex justify-center uppercase text-2xl border-y-2 border-x-2 border-zinc-600 mr-1 bg-stone-900 indent-2">
+                <div className='mt-2 flex'>
+                    <div className='mr-px'>
+                        {(text === 'Home') ?
+                            <div className='mt-1'>
+                                <AiFillHome size='25'/>
+                            </div> :
+                        (text === 'Map') ?
+                            <div className='mt-1.5'>
+                                <BsFillMapFill size='23'/>
+                            </div> :
+                        (text === 'Weather') ?
+                            <div className='mt-1'>
+                                <BsCloudSunFill />
+                            </div> :
+                        (text === 'About') ?
+                            <div className='mt-1'>
+                                <HiInformationCircle size='25' color='white'/>
+                            </div> :
+                            <></>         
+                        }
+                    </div>
+                    {text}
+                </div>
             </a>
         )
     };
@@ -28,7 +53,7 @@ export const Dropdown = (props) => {
             </div>
 
             {(toggle) ?
-            (<ul className={'absolute right-1 z-50 mt-1'}>
+            (<ul className={'absolute right-1 z-50 mt-1.5'}>
                 <li>
                     {
                         (props.choice === 'about') ? 
