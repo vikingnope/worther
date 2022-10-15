d36ad5c3195e4ce6eb78a9b58559cdf55da4f875
Aiden Schembri <aidsch0605@outlook.com>
2022-10-15 11:15:44 +0200

    fixed image width on mobile

--- src/pages/map.jsx
+++ src/pages/map.jsx
@@ -2,8 +2,9 @@ import React, {useState} from 'react';
 import { Header } from '../components/utils/header';
 import { Footer } from '../components/utils/footer';
 import { useNavigate } from 'react-router-dom';
-import lightModeImage from '../resources/lightMode.png'
-import darkModeImage from '../resources/darkMode.png'
+import lightModeImage from '../resources/lightMode.png';
+import darkModeImage from '../resources/darkMode.png';
+import { isMobile, isDesktop } from 'react-device-detect';
 
 export default function Home() {
 
@@ -28,13 +29,21 @@ export default function Home() {
                 <form onSubmit={handleSubmitLight}>
                     <button className='underline inline' type='submit'>
                         Light Mode Map
-                        <img src={lightModeImage} draggable='false' className="mx-auto mt-5 rounded-md max-w-xl border-2 border-zinc-600" />
+                        {
+                        (isDesktop) ?
+                            <img src={lightModeImage} draggable='false' className="mx-auto mt-5 rounded-md max-w-xl border-2 border-zinc-600" /> :
+                            <img src={lightModeImage} draggable='false' className="mx-auto mt-5 rounded-md max-w-sm border-2 border-zinc-600" />
+                        }
                     </button>   
                 </form>
                 <form onSubmit={handleSubmitDark}>
                     <button className='underline inline mt-12' type='submit'>
                         Dark Mode Map
-                        <img src={darkModeImage} draggable='false' className="mx-auto mt-5 mb-5 rounded-md max-w-xl border-2 border-zinc-600" />
+                        {
+                        (isDesktop) ?
+                            <img src={darkModeImage} draggable='false' className="mx-auto mt-5 rounded-md max-w-xl border-2 border-zinc-600" /> :
+                            <img src={darkModeImage} draggable='false' className="mx-auto mt-5 rounded-md max-w-sm border-2 border-zinc-600" />
+                        }
                     </button>
                 </form>
             </div>
