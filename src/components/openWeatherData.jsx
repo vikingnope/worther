496f6a4315a85e503c5bbc936d01c0b6aa9df985
Aiden Schembri <aidsch0605@outlook.com>
2022-10-16 13:05:39 +0200

    reduced code and fixed API calling twice bug

--- src/components/openWeatherData.jsx
+++ src/components/openWeatherData.jsx
@@ -29,42 +29,24 @@ export const GetOpenWeatherData = () => {
     const handleSubmit = (e) => {
       e.preventDefault();
   
-      history('/weather/' + city + '/' + lat + '/' + lon);
+      history('/weather/' + city + '/' + weather.lat + '/' + weather.lon);
     }
 
-    const [ name, setName ] = useState();
-    const [ lat, setLat ] = useState();
-    const [ lon, setLon ] = useState();
-    const [ country, setCountry ] = useState();
-    const [ humidity, setHumidity ] = useState();
-    const [ temperature, setTemperature ] = useState();
-    const [ tempMax, setTempMax ] = useState();
-    const [ tempMin, setTempMin ] = useState();
-    const [ tempFeel, setTempFeel ] = useState();
-    const [ pressure, setPressure ] = useState();
-    const [ mainWeather, setMainWeather ] = useState();
-    const [ description, setDescription ] = useState('');
-    const [ windSpeed, setWindSpeed ] = useState();
-    const [ windDegrees, setWindDegrees ] = useState();
-    const [ sunrise, setSunrise ] = useState();
-    const [ sunset, setSunset ] = useState();
-    const [ windDirection, setWindDirection ] = useState();
-    const [ visibility, setVisibility ] = useState();
+    const [ location, setLocation ] = useState([]);
+    const [ weather, setWeather ] = useState([]);
     const [ visibilityDescription, setVisibilityDescription ] = useState();
+    const [ windDirection, setWindDirection ] = useState();
+    const [ times, setTimes ] = useState([]);
     const [ timeZone, setTimeZone ] = useState();
     const [ loaded, setLoaded ] = useState();
-    const [ timeUpdatedUNIX, setTimeUpdatedUNIX ] = useState();
-    const [ sunriseTime, setSunriseTime ] = useState({hour: undefined, minute: undefined});
-    const [ sunsetTime, setSunsetTime ] = useState({hour: undefined, minute: undefined});
-    const [ timeUpdated, setTimeUpdated ] = useState({hour: undefined, minute: undefined});
     const [ blocked, setBlocked ] = useState();
 
-    sunriseTimeConversion = new Date(sunrise * 1000);
-    sunsetTimeConversion = new Date(sunset * 1000);
-    timeUpdatedConversion = new Date(timeUpdatedUNIX * 1000);
+    sunriseTimeConversion = new Date(weather.sunrise * 1000);
+    sunsetTimeConversion = new Date(weather.sunset * 1000);
+    timeUpdatedConversion = new Date(weather.timeUpdatedUNIX * 1000);
 
-    ((name) ?
-    document.title = "Worther - Weather - " + name :
+    ((location.name) ?
+    document.title = "Worther - Weather - " + location.name :
     document.title = "Worther - Weather");
 
     useEffect(() => {
@@ -74,75 +56,82 @@ export const GetOpenWeatherData = () => {
       (`https://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}&appid=${APIkey}&units=metric`) :
       (`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${APIkey}&units=metric`))
       .then(response => {
-        setName(response.data.name);
-        setLat(response.data.coord.lat);
-        setLon(response.data.coord.lon);
-        setCountry(response.data.sys.country);
-        setHumidity(response.data.main.humidity);
-        setTemperature(response.data.main.temp);
-        setTempMax(response.data.main.temp_max);
-        setTempMin(response.data.main.temp_min);
-        setTempFeel(response.data.main.feels_like);
-        setPressure(response.data.main.pressure);
-        setMainWeather(response.data.weather[0].main);
-        setDescription(response.data.weather[0].description);
-        setWindSpeed(response.data.wind.speed);
-        setWindDegrees(response.data.wind.deg);
-        setSunrise(response.data.sys.sunrise);
-        setSunset(response.data.sys.sunset);
-        setVisibility(response.data.visibility);
-        setTimeUpdatedUNIX(response.data.dt);
+        const weatherObj = {
+          humidity: response.data.main.humidity,
+          temperature: response.data.main.temp,
+          tempMax: response.data.main.temp_max,
+          tempMin: response.data.main.temp_min,
+          tempFeel: response.data.main.feels_like,
+          pressure: response.data.main.pressure,
+          mainWeather: response.data.weather[0].main,
+          description: response.data.weather[0].description,
+          windSpeed: response.data.wind.speed,
+          windDegrees: response.data.wind.deg,
+          sunrise: response.data.sys.sunrise,
+          sunset: response.data.sys.sunset,
+          visibility: response.data.visibility,
+          timeUpdatedUNIX: response.data.dt
+        }
+        setWeather(weatherObj);
 
-        ((visibility < 50) ?
+        const locationObj = {
+          name: response.data.name,
+          lat: response.data.coord.lat,
+          lon: response.data.coord.lon,
+          country: response.data.sys.country
+        }
+        setLocation(locationObj);
+
+        ((weather.visibility < 50) ?
           setVisibilityDescription('Dense Fog') :
-        (visibility >= 50 && visibility < 200) ?
+        (weather.visibility >= 50 && weather.visibility < 200) ?
           setVisibilityDescription('Thick Fog') :
-        (visibility >= 200 && visibility < 500) ?
+        (weather.visibility >= 200 && weather.visibility < 500) ?
           setVisibilityDescription('Moderate Fog') :
-        (visibility >= 500 && visibility < 1000) ?
+        (weather.visibility >= 500 && weather.visibility < 1000) ?
           setVisibilityDescription('Light Fog') :
-        (visibility >= 1000 && visibility < 2000) ?
+        (weather.visibility >= 1000 && weather.visibility < 2000) ?
           setVisibilityDescription('Thin Fog') :
-        (visibility >= 2000 && visibility < 4000) ?
+        (weather.visibility >= 2000 && weather.visibility < 4000) ?
           setVisibilityDescription('Haze') :
-        (visibility >= 4000 && visibility < 10000) ?
+        (weather.visibility >= 4000 && weather.visibility < 10000) ?
           setVisibilityDescription('Light Haze') :
-        (visibility === 10000) ?
+        (weather.visibility === 10000) ?
           setVisibilityDescription('Clear') :
           <></>
         );
 
-        (((windDegrees >= 0 && windDegrees <= 11.25) || (windDegrees  > 348.75)) ? 
+        (((weather.windDegrees >= 0 && weather.windDegrees <= 11.25) || (weather.windDegrees  > 348.75)) ? 
           setWindDirection('N')  : 
-        (windDegrees >= 11.26 && windDegrees < 33.75) ?
+        (weather.windDegrees >= 11.26 && weather.windDegrees < 33.75) ?
           setWindDirection('NNE') :
-        (windDegrees >= 33.75 && windDegrees < 56.25) ?
+        (weather.windDegrees >= 33.75 && weather.windDegrees < 56.25) ?
           setWindDirection('NE') :
-        (windDegrees >= 56.25 && windDegrees < 78.75) ?
+        (weather.windDegrees >= 56.25 && weather.windDegrees < 78.75) ?
           setWindDirection('ENE') :
-        (windDegrees >= 78.75 && windDegrees < 101.25) ?
+        (weather.windDegrees >= 78.75 && weather.windDegrees < 101.25) ?
           setWindDirection('E') :
-        (windDegrees >= 101.25 && windDegrees < 123.75) ?
+        (weather.windDegrees >= 101.25 && weather.windDegrees < 123.75) ?
           setWindDirection('ESE') :
-        (windDegrees >= 123.75 && windDegrees < 146.25) ?
+        (weather.windDegrees >= 123.75 && weather.windDegrees < 146.25) ?
           setWindDirection('SE') :
-        (windDegrees >= 146.25 && windDegrees < 168.75) ?
+        (weather.windDegrees >= 146.25 && weather.windDegrees < 168.75) ?
           setWindDirection('SSE') :
-        (windDegrees >= 168.75 && windDegrees < 191.25) ?
+        (weather.windDegrees >= 168.75 && weather.windDegrees < 191.25) ?
           setWindDirection('S') : 
-        (windDegrees >= 191.25 && windDegrees < 213.75) ?
+        (weather.windDegrees >= 191.25 && weather.windDegrees < 213.75) ?
           setWindDirection('SSW') :
-        (windDegrees >= 213.75 && windDegrees < 236.25) ?
+        (weather.windDegrees >= 213.75 && weather.windDegrees < 236.25) ?
           setWindDirection('SW') :
-        (windDegrees >= 236.25 && windDegrees < 258.75) ?
+        (weather.windDegrees >= 236.25 && weather.windDegrees < 258.75) ?
           setWindDirection('WSW') :
-        (windDegrees >= 258.75 && windDegrees < 281.25) ?
+        (weather.windDegrees >= 258.75 && weather.windDegrees < 281.25) ?
           setWindDirection('W') :
-        (windDegrees >= 281.25 && windDegrees < 303.75) ?
+        (weather.windDegrees >= 281.25 && weather.windDegrees < 303.75) ?
           setWindDirection('WNW') :
-        (windDegrees >= 303.75 && windDegrees < 326.25) ?
+        (weather.windDegrees >= 303.75 && weather.windDegrees < 326.25) ?
           setWindDirection('NW') :
-        (windDegrees >= 326.25 && windDegrees < 348.75) ?
+        (weather.windDegrees >= 326.25 && weather.windDegrees < 348.75) ?
           setWindDirection('NNW') :                          
         <> </>);
         
@@ -203,23 +192,16 @@ export const GetOpenWeatherData = () => {
         <></> 
         );
 
-        const newSunriseTime = { 
-          hour: String(sunriseTimeConversion.getHours()).padStart(2, '0'), // padStart makes sure we have 2 digits, if there is not it will add a 0 at the front
-          minute: String(sunriseTimeConversion.getMinutes()).padStart(2, '0')
-        };
-        setSunriseTime(newSunriseTime);
-  
-        const newSunsetTime = { 
-          hour: String(sunsetTimeConversion.getHours()).padStart(2, '0'), // padStart makes sure we have 2 digits, if there is not it will add a 0 at the front
-          minute: String(sunsetTimeConversion.getMinutes()).padStart(2, '0')
-        };
-        setSunsetTime(newSunsetTime);
-  
-        const newUpdatedTime = { 
-          hour: String(timeUpdatedConversion.getHours()).padStart(2, '0'), // padStart makes sure we have 2 digits, if there is not it will add a 0 at the front
-          minute: String(timeUpdatedConversion.getMinutes()).padStart(2, '0')
-        };
-        setTimeUpdated(newUpdatedTime);
+        const timesObj = {
+          sunriseHour: String(sunriseTimeConversion.getHours()).padStart(2, '0'), // padStart makes sure we have 2 digits, if there is not it will add a 0 at the front
+          sunriseMinute: String(sunriseTimeConversion.getMinutes()).padStart(2, '0'),
+          sunsetHour: String(sunsetTimeConversion.getHours()).padStart(2, '0'),
+          sunsetMinute: String(sunsetTimeConversion.getMinutes()).padStart(2, '0'),
+          timeUpdatedHour: String(timeUpdatedConversion.getHours()).padStart(2, '0'),
+          timeUpdatedMinute: String(timeUpdatedConversion.getMinutes()).padStart(2, '0')
+        }
+        setTimes(timesObj);
+      
         setLoaded(true);
       })
       .catch(error => {
@@ -229,58 +211,58 @@ export const GetOpenWeatherData = () => {
         )
         setLoaded(false);
       })
-    }, [!timeUpdated.hour]); // Calls API twice (needs fixing)
+    }, [!times]);
 
     return(
     <div className="text-white">
-      <Header choice={'showWeather'}/> 
+      <Header choice={'showWeather'}/>
       <div className="text-center select-none bg-black min-h-screen flex flex-col justify-center">
         {(loaded) ?
-          ((mainWeather) ?
+          ((weather.mainWeather) ?
             <>
               <section className="mx-auto mb-4">
-                {(mainWeather === "Clear") ?
+                {(weather.mainWeather === "Clear") ?
                   <BsFillSunFill size={'200'} color={'white'} /> :
-                (description === "scattered clouds" || description === "broken clouds") ?
+                (weather.description === "scattered clouds" || weather.description === "broken clouds") ?
                   <BsFillCloudSunFill size={'200'} color={'white'} className="mb-0" /> :
-                (description === "few clouds") ?
+                (weather.description === "few clouds") ?
                   <AiFillCloud size={'200'} color={'white'} className="mb-0" /> :
-                (description === "overcast clouds") ?
+                (weather.description === "overcast clouds") ?
                   <BsFillCloudsFill size={'200'} color={'white'} className="mb-0" /> :
-                (description === "light rain") ?
+                (weather.description === "light rain") ?
                   <BsFillCloudRainFill size={'200'} color={'white'} /> :
-                (description === "heavy intensity rain" || description === "moderate rain") ?
+                (weather.description === "heavy intensity rain" || weather.description === "moderate rain") ?
                   <BsFillCloudRainHeavyFill size={'200'} color={'white'} /> :
-                (mainWeather === "Drizzle") ?
+                (weather.mainWeather === "Drizzle") ?
                   <BsFillCloudDrizzleFill size={'200'} color={'white'} /> :
-                (description === "thunderstorm with light rain") ?
+                (weather.description === "thunderstorm with light rain") ?
                   <BsFillCloudLightningRainFill  size={'200'} color={'white'} className="mb-0" /> :
-                (mainWeather === "Fog") ?
+                (weather.mainWeather === "Fog") ?
                   <BsCloudFog size={'200'} color={'white'} /> :
-                (mainWeather === "Snow") ?
+                (weather.mainWeather === "Snow") ?
                   <BsFillCloudSnowFill size={'200'} color={'white'} /> :
                 <> </>}
 
               </section>
               <section className="text-lg">
-                <p className="underline text-3xl font-bold">{name}, {country}</p>
-                <p className="font-bold text-3xl mt-4">{description.toUpperCase()}</p>
-                <p className="mt-1">Temperature: {Math.round(temperature)}°C</p>
-                <p>Feels like: {Math.round(tempFeel)}°C</p>
-                <p>Max: {Math.round(tempMax)}°C &emsp; Min: {Math.round(tempMin)}°C</p>
-                <p>Humidity: {humidity}%</p>
-                <p>Wind Speed: {windSpeed} m/s &emsp; Wind Direction: {windDirection} @ {windDegrees}°</p>
-                <p>Pressure: {pressure} hPa</p>
-                <p>Visibility: {(visibility >= 1000) ?
-                  (visibility / 1000) + 'km' :
-                  (visibility) + 'm'} ({visibilityDescription})
+                <p className="underline text-3xl font-bold">{location.name}, {location.country}</p>
+                <p className="font-bold text-3xl mt-4">{weather.description.toUpperCase()}</p>
+                <p className="mt-1">Temperature: {Math.round(weather.temperature)}°C</p>
+                <p>Feels like: {Math.round(weather.tempFeel)}°C</p>
+                <p>Max: {Math.round(weather.tempMax)}°C &emsp; Min: {Math.round(weather.tempMin)}°C</p>
+                <p>Humidity: {weather.humidity}%</p>
+                <p>Wind Speed: {weather.windSpeed} m/s &emsp; Wind Direction: {weather.windDirection} @ {weather.windDegrees}°</p>
+                <p>Pressure: {weather.pressure} hPa</p>
+                <p>Visibility: {(weather.visibility >= 1000) ?
+                  (weather.visibility / 1000) + 'km' :
+                  (weather.visibility) + 'm'} ({visibilityDescription})
                 </p>
-                <p>Sunrise: {sunriseTime.hour}:{sunriseTime.minute} ({timeZone}) &emsp; Sunset: {sunsetTime.hour}:{sunsetTime.minute} ({timeZone})</p>
+                <p>Sunrise: {times.sunriseHour}:{times.sunriseMinute} ({timeZone}) &emsp; Sunset: {times.sunsetHour}:{times.sunsetMinute} ({timeZone})</p>
               </section><form onSubmit={handleSubmit}>
-                <button type='submit' className="text-lg underline mt-5 font-bold">Show 5 day weather</button>
+                <button type='submit' className="text-lg underline mt-5 font-bold">Show 3 hour weather</button>
               </form>
               <a className="text-xl mt-8 underline uppercase font-bold" href="/weather">Go Back</a>
-              <p className="absolute -bottom-12 right-1.5 underline">Last Updated: {timeUpdated.hour}:{timeUpdated.minute}</p>
+              <p className="absolute -bottom-12 right-1.5 underline">Last Updated: {times.timeUpdatedHour}:{times.timeUpdatedMinute}</p>
           </> :
           <>
             <p className="text-3xl uppercase font-bold">The city you have entered ('{city}') has not been found</p>
@@ -292,7 +274,7 @@ export const GetOpenWeatherData = () => {
             <p className="text-4xl uppercase font-bold">The API is currently blocked</p>
             <a className="text-xl mt-8 underline uppercase font-bold" href="/weather">Go Back</a>
           </> :
-          (loaded === false && !mainWeather) ?
+          (loaded === false && !weather.mainWeather) ?
             <>
               <p className="text-3xl uppercase font-bold">The city you have entered ('{city}') has not been found</p>
               <a className="text-xl mt-8 underline uppercase font-bold" href="/weather">Go Back</a>
