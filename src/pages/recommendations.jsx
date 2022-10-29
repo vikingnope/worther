92cd7825b963610e7bb19d0d1f702fe23eba4455
Aiden Schembri <aidsch0605@outlook.com>
2022-10-29 12:44:26 +0200

    Added map in recommendations

--- src/pages/recommendations.jsx
+++ src/pages/recommendations.jsx
@@ -1,12 +1,24 @@
 import { Header } from "../components/utils/header";
 import { Footer  } from "../components/utils/footer";
+import { MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
 
 export default function Recommendations () {
   return (
     <div className='select-none text-white'>
         <Header choice={'recommendations'}/>
         <div className="text-center bg-black flex min-h-screen flex-col">
-            <p className="text-5xl font-bold underline mt-3">Recommendations</p>
+          <p className="text-5xl font-bold underline mt-3 mb-8">Recommendations</p>
+          <div className="my-auto justify-center flex rounded">
+            <MapContainer center={[35.940125, 14.374125]} zoomControl={false} zoom={11.26} style={{ height: '82vh', width: '110vh', borderRadius: '6px'}} doubleClickZoom={false} scrollWheelZoom={false} closePopupOnClick= {false} dragging = {false} zoomSnap = {false} zoomDelta = {false} trackResize = {false} touchZoom = {false}>
+              <TileLayer zIndex={1}
+                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                  url={"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"}
+              />
+            </MapContainer>
+          </div>
+          <div className="mt-8 border-t-2 border-white w-full h-72">
+            
+          </div>
         </div>
         <Footer />
     </div>
