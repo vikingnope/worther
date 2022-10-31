005c7405f65ff9294c02cde283c589bc06c82119
vikingnope <aidsch0605@outlook.com>
2022-10-31 12:02:05 +0100

    Removed import and changed variables

--- src/pages/recommendations.jsx
+++ src/pages/recommendations.jsx
@@ -1,7 +1,6 @@
 import { Header } from "../components/utils/header";
 import { Footer  } from "../components/utils/footer";
 import { MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
-import maltaMap from '../resources/maltaMap.png';
 import L from 'leaflet';
 import markerDot from "../resources/location-dot.png";
 
@@ -22,7 +21,7 @@ export default function Recommendations () {
         <div className="text-center bg-black flex min-h-screen flex-col">
           <p className="text-5xl font-bold underline mt-3 mb-8">Recommendations</p>
           <section className="my-auto justify-center flex">
-            <MapContainer center={[35.940125, 14.374125]} zoom={11} minZoom={11} style={{ height: '82vh', width: '110vh', borderRadius: '7px'}} maxBounds={[[36.237281, 13.910769], [35.641324,14.802748]]} maxBoundsViscosity={0.75} doubleClickZoom={false}>
+            <MapContainer center={[35.940125, 14.374125]} zoom={11} minZoom={11} style={{ height: '82vh', width: '110vh', borderRadius: '7px'}} maxBounds={[[36.177098, 14.014540], [35.641324,14.802748]]} maxBoundsViscosity={1} doubleClickZoom={false}>
               <TileLayer zIndex={1}
                   attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                   url={"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"}
