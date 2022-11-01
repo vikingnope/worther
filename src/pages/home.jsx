ad86d01791899440e7c8057b3b83a45e2a5801e2
vikingnope <aidsch0605@outlook.com>
2022-11-01 22:18:13 +0100

    Started change of home screen

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,7 @@
 import logo from '../resources/logo.png';
 import { Header } from '../components/utils/header';
 import { Footer } from '../components/utils/footer';
+import lightMode from '../resources/lightMode.png';
 
 
 export default function Home() {
@@ -9,7 +10,9 @@ export default function Home() {
             <Header choice={'home'}/>
             <div className="text-center bg-black flex min-h-screen flex-col items-center justify-center">
                 <main>
-                    <img src={logo} className="mx-auto mb-7 App-logo rounded-full" alt="logo" />
+                    <a href='/map'>
+                        <img src={lightMode} className="mb-7 rounded border-2 hover:border-red-800 opacity-90 w-3/4 hover:w-4/5 duration-300 h-4/5" alt="map" />
+                    </a>
                     <p className="uppercase font-bold text-7xl">
                         Worther
                     </p>
