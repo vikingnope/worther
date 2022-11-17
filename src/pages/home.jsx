28372ce57db321da4b818bed3d7aa73e91408a43
Aiden Schembri <aidsch0605@outlook.com>
2022-11-17 13:14:15 +0100

    Cleaned code further

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,5 @@
 import { Header } from '../components/utils/header';
 import { Footer } from '../components/utils/footer';
-import lightMode from '../resources/lightMode.png';
 
 export default function Home() {
     
@@ -14,13 +13,11 @@ export default function Home() {
                 <p className="uppercase text-3xl mt-7 mb-10">
                     Getting weather closer to you
                 </p>
-                <ul>
+                <ul className='my-16'>
                     <a href='/map' className='uppercase text-5xl underline block w-max mx-auto font-bold hover:text-cyan-300 hover:text-5xl duration-500 mb-5'>Map</a>
                     <a href='/weather' className='uppercase text-5xl underline w-max mx-auto block font-bold hover:text-cyan-300 hover:text-5xl duration-500 mb-5'>Weather</a>
                     <a href='/recommendations' className='uppercase text-5xl block underline w-max mx-auto font-bold hover:text-cyan-300 hover:text-5xl duration-500'>Recommendations</a>
                 </ul>
-                
-
             </div>
             <Footer />
         </div>
