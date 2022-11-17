33e5322bed06ab4b4d023a71ec5e2b81764955ff
Aiden Schembri <aidsch0605@outlook.com>
2022-11-17 13:25:59 +0100

    updated styling

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -7,16 +7,16 @@ export default function Home() {
         <div className='select-none text-white'>
             <Header choice={'home'}/>
             <div className="text-center bg-black flex min-h-screen flex-col items-center">
-                <p className="uppercase font-bold text-7xl mt-10">
+                <p className="uppercase font-bold text-7xl mt-20 text-green-500">
                     Worther
                 </p>
-                <p className="uppercase text-3xl mt-7 mb-10">
+                <p className="uppercase text-3xl mt-7 mb-10 text-sky-600">
                     Getting weather closer to you
                 </p>
                 <ul className='my-16'>
-                    <a href='/map' className='uppercase text-5xl underline block w-max font-bold hover:text-cyan-300 hover:text-5xl duration-500 mb-10'>Map</a>
-                    <a href='/weather' className='uppercase text-5xl underline w-max block font-bold hover:text-cyan-300 hover:text-5xl duration-500 mb-10'>Weather</a>
-                    <a href='/recommendations' className='uppercase text-5xl block underline w-max font-bold hover:text-cyan-300 hover:text-5xl duration-500'>Recommendations</a>
+                    <a href='/map' className='uppercase text-5xl underline block w-max font-bold hover:text-cyan-300 hover:text-6xl duration-500 mb-10'>Map</a>
+                    <a href='/weather' className='uppercase text-5xl underline w-max block font-bold hover:text-cyan-300 hover:text-6xl duration-500 mb-10'>Weather</a>
+                    <a href='/recommendations' className='uppercase text-5xl block underline w-max font-bold hover:text-cyan-300 hover:text-6xl duration-500'>Recommendations</a>
                 </ul>
             </div>
             <Footer />
