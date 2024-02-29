29b3f29d06f94fac8da0dd851b2af669bd144d2a
vikingnope <aidsch0605@outlook.com>
2024-02-29 21:21:19 +0100

    changed map to be a button to choose between dark mode and light mode

---
+++ src/components/utils/mapMode.jsx
@@ -0,0 +1,34 @@
+import { MdOutlineLightMode } from "react-icons/md";
+import { MdDarkMode } from "react-icons/md";
+import { useNavigate } from 'react-router-dom';
+
+export const MapMode = (props) => {
+    const history = useNavigate();
+
+    const handleSubmitDark = (e) => {
+        e.preventDefault();
+
+        history('/map/dark');
+    }
+
+    const handleSubmitLight = (e) => {
+        e.preventDefault();
+
+        history('/map/light');
+    }
+
+    return (
+        <div className="z-50 absolute bottom-12 right-8">
+            {
+                (props.mode === 'light') ?
+                    <button onClick={handleSubmitDark} className="bg-gray-800 text-white p-2 rounded-lg" title="Dark Mode">
+                        <MdDarkMode size={27}/> 
+                    </button>
+                    : 
+                    <button onClick={handleSubmitLight} className="bg-neutral-300 text-white p-2 rounded-lg" title="Light Mode">
+                        <MdOutlineLightMode size={27} color="black"/>
+                    </button>
+            }
+        </div>
+    )
+}
--- src/pages/app.jsx
+++ src/pages/app.jsx
@@ -2,7 +2,6 @@ import '../styles/main.scss';
 import Home from './home.jsx';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import About from './about.jsx';
-import Map from './map.jsx';
 import Weather from './weather.jsx';
 import AdvancedWeather from './advancedWeather.jsx';
 import { GetOpenWeatherData } from '../components/openWeatherData.jsx';
@@ -21,7 +20,6 @@ export default function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/about" element={<About />} />
-          <Route exact path="/map" element={<Map />} />
           <Route exact path="/map/:mapType" element={<ShowMap />} />
           <Route exact path="/weather" element={<Weather />} />
           <Route exact path="/advancedWeather" element={<AdvancedWeather />} />
