e277b10bf2e3edb7fbd23cb31d04facf2d0ff227
vikingnope <aidsch0605@outlook.com>
2025-01-26 11:37:32 +0100

    Adjusted styling to be more editable

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,22 +3,24 @@ import { Footer } from '../components/utils/footer';
 
 export default function About() {
   document.title = "Worther - About";
+
+  const clickableTextStyling = 'underline duration-300 font-bold hover:text-cyan-300'
   
   return (
     <div className="select-none text-white overflow-hidden">
       <Header choice={'about'}/>
       <main className="bg-black flex flex-col items-center justify-center h-screen">
-          <p className="uppercase font-bold md:text-7xl text-5xl">
+          <p className="uppercase font-bold md:text-7xl text-5xl mb-14">
             Welcome to Worther!
           </p>
-          <p className="uppercase md:text-3xl text-2xl mt-8">
-            The weather app with the map of the world and current real time weather.
+          <p className="md:text-3xl text-2xl mt-8">
+            The weather app with current weather of the whole world.
           </p>
           <p className="md:text-3xl text-2xl mt-8">
-            Built using <a href="https://reactjs.org/" className="underline font-bold hover:text-cyan-300 duration-300">React</a>, <a href="https://reactrouter.com/en/main" className="underline font-bold hover:text-cyan-300 duration-300">React Router</a>, <a href="https://react-icons.github.io/react-icons" className="underline duration-300 font-bold hover:text-cyan-300">React Icons</a>, <a href="https://react-leaflet.js.org/" className="underline font-bold hover:text-cyan-300 duration-300">React Leaflet</a>, <a href="https://axios-http.com/docs/intro" className="underline font-bold hover:text-cyan-300 duration-300">Axios</a>, & <a href="https://tailwindcss.com/" className="underline font-bold hover:text-cyan-300 duration-300">Tailwind CSS</a>.
+            Built using <a href="https://reactjs.org/" className={clickableTextStyling}>React</a>, <a href="https://reactrouter.com/en/main" className={clickableTextStyling}>React Router</a>, <a href="https://react-icons.github.io/react-icons" className={clickableTextStyling}>React Icons</a>, <a href="https://react-leaflet.js.org/" className={clickableTextStyling}>React Leaflet</a>, <a href="https://axios-http.com/docs/intro" className={clickableTextStyling}>Axios</a>, & <a href="https://tailwindcss.com/" className={clickableTextStyling}>Tailwind CSS</a>.
           </p>
           <p className="md:text-3xl text-2xl mt-8">
-            APIs used: <a href="https://wiki.openstreetmap.org/wiki/API" className="underline font-bold hover:text-cyan-300 duration-300">OpenStreetMap API</a>, <a href="https://www.rainviewer.com/api.html" className="underline duration-300 font-bold hover:text-cyan-300">Rainviewer API</a>, <a href="https://openweathermap.org/api" className="underline font-bold hover:text-cyan-300 duration-300">OpenWeatherMap API</a>
+            APIs used: <a href="https://wiki.openstreetmap.org/wiki/API" className={clickableTextStyling}>OpenStreetMap API</a>, <a href="https://www.rainviewer.com/api.html" className={clickableTextStyling}>Rainviewer API</a>, <a href="https://openweathermap.org/api" className={clickableTextStyling}>OpenWeatherMap API</a>
           </p>
           <p className="absolute text-base md:-bottom-12 -bottom-2 left-1.5">
           <span className='underline'>Disclaimer:</span> Any location data used is only sent to the weather API when search by location is chosen, on the map it is only kept on the client side. Analytics are also sent to a server.
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,6 +2,9 @@ import { Header } from '../components/utils/header';
 import { Footer } from '../components/utils/footer';
 
 export default function Home() {
+
+    const siteMapStyling = 'uppercase md:text-5xl text-3xl underline block w-max font-bold hover:text-cyan-300 duration-300'
+
     return (
         <div className='select-none text-white overflow-hidden'>
             <Header choice={'home'}/>
@@ -9,14 +12,14 @@ export default function Home() {
                 <p className="uppercase font-bold md:text-8xl text-5xl mt-20 text-green-500">
                     Worther
                 </p>
-                <p className="uppercase md:text-5xl text-3xl mt-7 mb-10 text-sky-400">
+                <p className="uppercase md:text-5xl text-2xl mt-7 mb-10 text-sky-400">
                     Getting weather closer to you
                 </p>
                 <section className='my-16'>
-                    <a href='/map/light' className='uppercase md:text-5xl text-3xl underline block w-max font-bold hover:text-cyan-300 hover:text-6xl duration-500 md:mb-10 mb-5'>Map</a>
-                    <a href='/weather' className='uppercase md:text-5xl text-3xl underline w-max block font-bold hover:text-cyan-300 hover:text-6xl duration-500 md:mb-10 mb-5'>Weather</a>
-                    <a href='/recommendations' className='uppercase md:text-5xl text-3xl block underline w-max font-bold hover:text-cyan-300 hover:text-6xl duration-500 md:mb-10 mb-5'>Recommendations</a>
-                    <a href='/about' className='uppercase md:text-5xl text-3xl block underline w-max font-bold hover:text-cyan-300 hover:text-6xl duration-500'>About</a>
+                    <a href='/map/light' className={`${siteMapStyling} md:mb-10 mb-5`}>Map</a>
+                    <a href='/weather' className={`${siteMapStyling} md:mb-10 mb-5`}>Weather</a>
+                    <a href='/recommendations' className={`${siteMapStyling} md:mb-10 mb-5`}>Recommendations</a>
+                    <a href='/about' className={siteMapStyling}>About</a>
                 </section>
             </div>
             <Footer />
