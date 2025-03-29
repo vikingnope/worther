4613e4a8eb0b7e8212e2a363f2876e9fca626635
Aiden Schembri <aidsch0605@outlook.com>
2025-03-29 21:08:51 +0100

    optimised home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -8,20 +8,20 @@ export default function Home() {
     return (
         <div className='flex flex-col min-h-screen text-white overflow-hidden bg-black'>
             <Header/>
-            <div className="flex-grow text-center flex flex-col items-center justify-center">
-                <p className="uppercase font-bold md:text-8xl text-5xl text-green-500">
+            <main className="flex-grow text-center flex flex-col items-center justify-center">
+                <h1 className="uppercase font-bold md:text-8xl text-5xl text-green-500">
                     Worther
-                </p>
+                </h1>
                 <p className="uppercase md:text-5xl text-2xl mt-5 mb-10 text-sky-400">
                     Getting weather closer to you
                 </p>
-                <section className='my-10'>
+                <nav className='my-10' aria-label="Main navigation">
                     <a href='/map/light' className={`${siteMapStyling} md:mb-10 mb-5`}>Map</a>
                     <a href='/weather' className={`${siteMapStyling} md:mb-10 mb-5`}>Weather</a>
                     <a href='/recommendations' className={`${siteMapStyling} md:mb-10 mb-5`}>Recommendations</a>
                     <a href='/about' className={siteMapStyling}>About</a>
-                </section>
-            </div>
+                </nav>
+            </main>
             <Footer />
         </div>
     )
