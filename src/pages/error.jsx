ba1a93ed61f8cbe3f2a098bbf53b21b4133d12b1
Aiden Schembri <aidsch0605@outlook.com>
2025-03-29 21:14:45 +0100

    enhanced error page

--- src/pages/error.jsx
+++ src/pages/error.jsx
@@ -1,22 +1,30 @@
 import { Header } from '../components/utils/header';
 import { Footer } from '../components/utils/footer';
-
+import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Error() {
+    
+    useEffect(() => {
+        document.title = "Worther - Error 404";
+    }, []);
+
     return (
         <div className='text-white overflow-hidden bg-black'>
             <Header/>
-            <div className="text-center flex min-h-screen flex-col items-center justify-center">
-                <p className="font-bold text-5xl text-purple-400 mb-12">
+            <main className="text-center flex min-h-screen flex-col items-center justify-center">
+                <h1 className="font-bold text-5xl text-purple-400 mb-12">
                     Oops! This page has gone on holiday. It might return when it feels like it!
-                </p>
-                <p className="font-bold text-3xl text-amber-400">
+                </h1>
+
+                <h2 className="font-bold text-3xl text-amber-400">
                     Error 404: The URL you have entered has not been found.
-                </p>
-                <a href='/' className="uppercase underline font-bold text-2xl mt-20 hover:text-cyan-300 duration-300">
+                </h2>
+
+                <Link to='/' className="uppercase underline font-bold text-2xl mt-20 hover:text-cyan-300 duration-300" aria-label="Return to home page">
                     Go home
-                </a>
-            </div>
+                </Link>
+            </main>
             <Footer />
         </div>
     );
