cd6049130112f5628b6df2ae49bc565bd096b360
Aiden Schembri <aidsch0605@outlook.com>
2025-03-30 00:24:29 +0100

    enhanced footer

--- src/components/utils/footer.jsx
+++ src/components/utils/footer.jsx
@@ -1,19 +1,20 @@
 import { FaDiscord } from 'react-icons/fa';
 import { BsGithub } from 'react-icons/bs';
 import { IoDocumentTextSharp } from "react-icons/io5";
+import { memo } from 'react';
 
-export const Footer = () => {
+export const Footer = memo(() => {
     return(
         <footer className="inset-x-0 bottom-0 bg-neutral-800 h-10 w-full border-y border-zinc-600">
             <nav className="relative -bottom-1.5 flex gap-x-5 justify-center">
-                <a className="hover:text-green-300" href="https://discord.gg/n6xr3ZWM8J" target="_blank" rel="noreferrer">
+                <a className="hover:text-green-300" href="https://discord.gg/n6xr3ZWM8J" target="_blank" rel="noreferrer" aria-label="Join Discord server">
                     <FaDiscord size="26"/>
                 </a>
-                <a className="hover:text-green-300" href="https://github.com/vikingnope/worther" target="_blank" rel="noreferrer">
+                <a className="hover:text-green-300" href="https://github.com/vikingnope/worther" target="_blank" rel="noreferrer" aria-label="View GitHub repository">
                     <BsGithub size="26"/>
                 </a>
                 <div className='absolute right-5'>
-                    <a href='/changelog' className='font-bold underline flex gap-x-1'>
+                    <a href='/changelog' className='font-bold underline flex gap-x-1' aria-label="View changelog">
                         <IoDocumentTextSharp size="20"className='my-auto'/>
                         Changelog
                     </a>
@@ -21,5 +22,7 @@ export const Footer = () => {
             </nav>  
         </footer> 
     )
-};
+});
+
+Footer.displayName = 'Footer';
     
