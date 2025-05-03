7702f0d630a3c4ee6d5972eb36064ce4f7f01a41
Aiden Schembri <aidsch0605@outlook.com>
2025-05-03 20:33:54 +0200

    changed more imports to aliases

--- src/pages/app.jsx
+++ src/pages/app.jsx
@@ -1,34 +1,34 @@
-import '../styles/main.css';
+import '@styles/main.css';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/react';
 import { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 // Lazy load all components
-const Home = lazy(() => import('./home.jsx'));
-const About = lazy(() => import('./about.jsx'));
-const Weather = lazy(() => import('./weather.jsx'));
+const Home = lazy(() => import('@pages/home.jsx'));
+const About = lazy(() => import('@pages/about.jsx'));
+const Weather = lazy(() => import('@pages/weather.jsx'));
 const GetSingleWeather = lazy(() =>
-  import('../components/weatherSingle.jsx').then(module => ({ default: module.GetSingleWeather }))
+  import('@components/weatherSingle.jsx').then(module => ({ default: module.GetSingleWeather }))
 );
 const ThreeHourForecastData = lazy(() =>
-  import('../components/3HourForecastData.jsx').then(module => ({
+  import('@components/3HourForecastData.jsx').then(module => ({
     default: module.ThreeHourForecastData,
   }))
 );
-const ShowMap = lazy(() => import('./map.jsx'));
+const ShowMap = lazy(() => import('@pages/map.jsx'));
 const SingleThreeHourForecastData = lazy(() =>
-  import('../components/Single3HourForecastData.jsx').then(module => ({
+  import('@components/Single3HourForecastData.jsx').then(module => ({
     default: module.SingleThreeHourForecastData,
   }))
 );
-const ErrorPage = lazy(() => import('./error.jsx'));
-const Recommendations = lazy(() => import('./recommendations.jsx'));
+const ErrorPage = lazy(() => import('@pages/error.jsx'));
+const Recommendations = lazy(() => import('@pages/recommendations.jsx'));
 const DailyWeatherData = lazy(() =>
-  import('../components/dailyWeather.jsx').then(module => ({ default: module.DailyWeatherData }))
+  import('@components/dailyWeather.jsx').then(module => ({ default: module.DailyWeatherData }))
 );
-const Changelog = lazy(() => import('./changelog.jsx'));
-const Settings = lazy(() => import('./settings.jsx'));
+const Changelog = lazy(() => import('@pages/changelog.jsx'));
+const Settings = lazy(() => import('@pages/settings.jsx'));
 
 export default function App() {
   return (
